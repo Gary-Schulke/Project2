5f63a5315b63f00e6f301cdc4b96e01917e9f017
Lindsey McKenna <lin.mckenna@gmail.com>
2020-01-22 10:01:04 -0800

    Added color scale to bars and radius change for line chart markers

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -1,172 +1,230 @@
 //Capture file path for JSON file
-var jsonLoc = "/Project2/static/data/monthly_rider_data.json";
+var jsonRider = '/Project2/static/data/monthly_rider_data.json';
+var jsonWeather = '/Project2/static/data/final_weather_data.json';
+
+//Declare variables of the function that need to live outside of the for loop
+var allMonths = [];
+var allYears = [2017, 2018, 2019];
+var selectedMonth = "Jan";
+var selectedMonthYear = [];
+var selectedMonthBus = [];
+var selectedMonthMax = [];
+var selectedMonthWes = [];
+var selectedYear = 2017;
+var selectedYearDates = [];
+var selectedYearBus = [];
+var selectedYearMax = [];
+var selectedYearWes = [];
+var selectedMonthTotPrecip = [];
+var selectedYearTotPrecip = [];
+var selectedMonthPrecipColors = [];
+
+//Function for identifying where the total precipitation falls on the color scale
+var colorScale = ['#CCF5CE', '#AFEEBE', '#92E7B5', '#75DFB3', '#57D7B8', '#3ACEC3', '#1DB5C4', '#008CBA', '#00499F', '#001382']
+var numericScale = ['0"-5"', '6"-10"', '11"-15"', '16"-20"', '21"-25"', '26"-30"', '31"-35"', '36"-40"', '41"-45"', '45"+']
+
+function getColor(d) {
+    return d > 45 ? colorScale[9] :
+        d > 40 ? colorScale[8] :
+            d > 35 ? colorScale[7] :
+                d > 30 ? colorScale[6] :
+                    d > 25 ? colorScale[5] :
+                        d > 20 ? colorScale[4] :
+                            d > 15 ? colorScale[3] :
+                                d > 10 ? colorScale[2] :
+                                    d > 5 ? colorScale[1] :
+                                    colorScale[0]
+};
+
+//Add color scale for reference
+d3.select("#color-scale").selectAll('div')
+    .data(colorScale)
+    .enter()
+    .append('div')
+    .attr('class', '#color-block')
+    .style('width', '38px')
+    .style('height', '20px')
+    .style('padding-left', '2px')
+    .style('padding-top', '2px')
+    .style('margin-bottom', '50px')
+    .style('display', 'inline-block')
+    .style('background-color', (d => d))
+
+d3.select("#color-scale").selectAll('div')
+    .data(numericScale)
+    .text(d => d)
+    .style('font-size', "10px")
+    .style('color', '#fff')
+
 
 //Create function for pulling the data and pushing it into the charts
 function getInitialData() {
 
-    //Declare variables of the function that need to live outside of the for loop
-    // var allDates = [];
-    var allMonths = [];
-    var allYears = [2017, 2018, 2019];
-    // var allBus = [];
-    // var allMax = [];
-    // var allWes = [];
-    var selectedMonth = "Jan";
-    var selectedMonthYear = [];
-    var selectedMonthBus = [];
-    var selectedMonthMax = [];
-    var selectedMonthWes = [];
-    var selectedYear = 2017;
-    var selectedYearDates = [];
-    var selectedYearBus = [];
-    var selectedYearMax = [];
-    var selectedYearWes = [];
-
-
     //Read JSON file, then push all data needed into above variables
-    d3.json(jsonLoc).then(function (d) {
+    d3.json(jsonWeather).then(function (d) {
+
         for (var i = 0; i < d.data.length; i++) {
-            // allDates.push(d.data[i].date);
-            allMonths.push(d.data[i].month);
-            // allBus.push(d.data[i].bus);
-            // allMax.push(d.data[i].max);
-            // allWes.push(d.data[i].wes);
             if (d.data[i].month === selectedMonth) {
-                selectedMonthYear.push(d.data[i].year);
-                selectedMonthBus.push(d.data[i].bus);
-                selectedMonthMax.push(d.data[i].max);
-                selectedMonthWes.push(d.data[i].wes);
+                selectedMonthTotPrecip.push(d.data[i].total_precip);
+                selectedMonthPrecipColors.push(getColor(d.data[i].total_precip));
             };
             if (d.data[i].year === selectedYear) {
-                selectedYearDates.push(d.data[i].date);
-                selectedYearBus.push(d.data[i].bus);
-                selectedYearMax.push(d.data[i].max);
-                selectedYearWes.push(d.data[i].wes);
+                selectedYearTotPrecip.push(d.data[i].total_precip);
             };
         };
+        console.log(selectedMonthTotPrecip);
+        console.log(selectedYearTotPrecip);
 
-        //Add Months to dropdown menu
-        var dropDownMonths = allMonths.slice(0, 12).forEach(d => d3.select("#selDataset1").append("option").attr("value", d).text(d));
+        d3.json(jsonRider).then(function (d) {
+            for (var i = 0; i < d.data.length; i++) {
 
-        //Add Years to dropdown menu
-        var dropDownYears = allYears.forEach(d => d3.select("#selDataset2").append("option").attr("value", d).text(d));
+                allMonths.push(d.data[i].month);
 
-        //Define the coordinates for the bus chart and identify the chart type as bar
-        var busBarTrace = {
-            x: selectedMonthYear,
-            y: selectedMonthBus,
-            name: 'Bus',
-            type: 'bar'
-        };
+                if (d.data[i].month === selectedMonth) {
+                    selectedMonthYear.push(d.data[i].year);
+                    selectedMonthBus.push(d.data[i].bus);
+                    selectedMonthMax.push(d.data[i].max);
+                    selectedMonthWes.push(d.data[i].wes);
+                };
+                if (d.data[i].year === selectedYear) {
+                    selectedYearDates.push(d.data[i].date);
+                    selectedYearBus.push(d.data[i].bus);
+                    selectedYearMax.push(d.data[i].max);
+                    selectedYearWes.push(d.data[i].wes);
+                };
+            };
 
-        //Store the trace for the bus bar chart
-        var busBarData = [busBarTrace];
-        
-        //Add layout properties for the bus bar chart
-        var busBarLayout = { title: 'Monthly Bus Riders' };
+            //Add Months to dropdown menu
+            var dropDownMonths = allMonths.slice(0, 12).forEach(d => d3.select("#selDataset1").append("option").attr("value", d).text(d));
 
-        //Create the bus bar chart
-        Plotly.newPlot('bar-bus', busBarData, busBarLayout);
+            //Add Years to dropdown menu
+            var dropDownYears = allYears.forEach(d => d3.select("#selDataset2").append("option").attr("value", d).text(d));
 
-        //Define the coordinates for the MAX chart and identify the chart type as bar
-        var maxBarTrace = {
-            x: selectedMonthYear,
-            y: selectedMonthMax,
-            name: 'MAX',
-            type: 'bar'
-        };
+            //Define the coordinates for the bus chart and identify the chart type as bar
+            var busBarTrace = {
+                x: selectedMonthYear,
+                y: selectedMonthBus,
+                name: 'Bus',
+                type: 'bar',
+                marker: {
+                    color: selectedMonthPrecipColors
+                }
+            };
 
-        //Store the trace for the MAX bar chart
-        var maxBarData = [maxBarTrace];
+            //Store the trace for the bus bar chart
+            var busBarData = [busBarTrace];
 
-        //Add layout properties for the MAX bar chart
-        var maxBarLayout = { title: 'Monthly MAX Riders' };
+            //Add layout properties for the bus bar chart
+            var busBarLayout = { title: 'Monthly Bus Riders' };
 
-        //Create the MAX bar chart
-        Plotly.newPlot('bar-max', maxBarData, maxBarLayout);
+            //Create the bus bar chart
+            Plotly.newPlot('bar-bus', busBarData, busBarLayout);
 
-        //Define the coordinates for the WES chart and identify the chart type as bar
-        var wesBarTrace = {
-            x: selectedMonthYear,
-            y: selectedMonthWes,
-            name: 'WES',
-            type: 'bar'
-        };
+            //Define the coordinates for the MAX chart and identify the chart type as bar
+            var maxBarTrace = {
+                x: selectedMonthYear,
+                y: selectedMonthMax,
+                name: 'MAX',
+                type: 'bar',
+                marker: {
+                    color: selectedMonthPrecipColors
+                }
+            };
 
-        //Store the trace for the WES bar chart
-        var wesBarData = [wesBarTrace];
+            //Store the trace for the MAX bar chart
+            var maxBarData = [maxBarTrace];
 
-        //Add layout properties for the WES bar chart
-        var wesBarLayout = { title: 'Monthly WES Riders' };
+            //Add layout properties for the MAX bar chart
+            var maxBarLayout = { title: 'Monthly MAX Riders' };
 
-        //Create the WES bar chart
-        Plotly.newPlot('bar-wes', wesBarData, wesBarLayout);
+            //Create the MAX bar chart
+            Plotly.newPlot('bar-max', maxBarData, maxBarLayout);
 
-        //Define the coordinates for the yearly line chart and identify the chart type as line
-        //Create the bus trace
-        var traceBusLine = {
-            x: selectedYearDates,
-            y: selectedYearBus,
-            name: 'Bus',
-            type: 'scatter',
-            marker: {
-                color: '#008cba',
-                size: 10,
-                // line: {
-                //   color: '#008cba',
-                //   width: 2
-                // }
-            }
-        };
+            //Define the coordinates for the WES chart and identify the chart type as bar
+            var wesBarTrace = {
+                x: selectedMonthYear,
+                y: selectedMonthWes,
+                name: 'WES',
+                type: 'bar',
+                marker: {
+                    color: selectedMonthPrecipColors
+                }
+            };
 
-        //Create the MAX trace
-        var traceMaxLine = {
-            x: selectedYearDates,
-            y: selectedYearMax,
-            name: 'MAX',
-            type: 'scatter',
-            marker: {
-                color: '#20c997',
-                size: 10,
-                // symbol: ["diamond"],
-                // line: {
-                //   color: '#20c997',
-                //   width: 2
-                // }
-            }
-        };
+            //Store the trace for the WES bar chart
+            var wesBarData = [wesBarTrace];
 
-        //Create the WES trace
-        var traceWesLine = {
-            x: selectedYearDates,
-            y: selectedYearWes,
-            name: 'WES',
-            type: 'scatter',
-            marker: {
-                color: '#43ac6a',
-                size: 10,
-                // symbol: ["square"],
-                // line: {
-                //   color: '#43ac6a',
-                //   width: 2
-                // }
-            }
-        };
+            //Add layout properties for the WES bar chart
+            var wesBarLayout = { title: 'Monthly WES Riders' };
 
-        var lineLayout = {
-            legend: {
-                x: 1,
-                xanchor: 'right',
-                y: 1
-            },
-          };
+            //Create the WES bar chart
+            Plotly.newPlot('bar-wes', wesBarData, wesBarLayout);
 
-        //Store all traces for the yearly line chart
-        var yearlyData = [traceBusLine, traceMaxLine, traceWesLine];
+            //Define the coordinates for the yearly line chart and identify the chart type as line
+            //Create the bus trace
+            var traceBusLine = {
+                x: selectedYearDates,
+                y: selectedYearBus,
+                name: 'Bus',
+                type: 'scatter',
+                marker: {
+                    color: '#008cba',
+                    size: selectedYearTotPrecip,
+                    // line: {
+                    //   color: '#008cba',
+                    //   width: 2
+                    // }
+                }
+            };
 
-        //Create the yearly line chart
-        Plotly.newPlot('line', yearlyData, lineLayout);
+            //Create the MAX trace
+            var traceMaxLine = {
+                x: selectedYearDates,
+                y: selectedYearMax,
+                name: 'MAX',
+                type: 'scatter',
+                marker: {
+                    color: '#20c997',
+                    size: selectedYearTotPrecip,
+                    // symbol: ["diamond"],
+                    // line: {
+                    //   color: '#20c997',
+                    //   width: 2
+                    // }
+                }
+            };
+
+            //Create the WES trace
+            var traceWesLine = {
+                x: selectedYearDates,
+                y: selectedYearWes,
+                name: 'WES',
+                type: 'scatter',
+                marker: {
+                    color: '#43ac6a',
+                    size: selectedYearTotPrecip,
+                    // symbol: ["square"],
+                    // line: {
+                    //   color: '#43ac6a',
+                    //   width: 2
+                    // }
+                }
+            };
+
+            // var lineLayout = {
+            //     legend: {
+            //         x: 1,
+            //         xanchor: 'right',
+            //         y: 1
+            //     },
+            // };
+
+            //Store all traces for the yearly line chart
+            var yearlyData = [traceBusLine, traceMaxLine, traceWesLine];
+
+            //Create the yearly line chart
+            Plotly.newPlot('line', yearlyData);
+        });
     });
-    console.log(selectedYearDates);
 };
 getInitialData();
