abbca2375d6638a12dfbb06aa2a2b9d06bbf1fe3
Lindsey McKenna <lin.mckenna@gmail.com>
2020-01-22 21:37:48 -0800

    Updated file path

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -1,6 +1,6 @@
 //Capture file path for JSON file
-var jsonRider = '/static/data/monthly_rider_data.json';
-var jsonWeather = '/static/data/final_weather_data.json';
+var jsonRider = './static/data/monthly_rider_data.json';
+var jsonWeather = './static/data/final_weather_data.json';
 
 //Declare variables of the function that need to live outside of the for loop
 var allMonths = [];
