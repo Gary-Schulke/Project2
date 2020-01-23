9347519c700f13f15d14e9ec6d41c6a41bbc8ba2
gary-schulke <gary.schulke@outlook.com>
2020-01-22 21:15:29 -0800

    updated

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 // API key
-const API_KEY = "pk.eyJ1IjoidGFiZXJjd2ViYiIsImEiOiJjazR6dms1aHMwYm5uM25xaDJzazRwdG9oIn0.YHjPZbvmBdL8JhOfqQ9gJQ";
+const API_KEY = "pk.eyJ1IjoiZ2FyeXNjaHVsa2UiLCJhIjoiY2s0enc4bnpyMDg5NTNtbzB5d3RkNGExOSJ9.RL2z_jZO5LmlNuw-s4A7jw";
 
-const MAPY = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
+const MAPY = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
 
 const ATTRIBUTION = "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>";
